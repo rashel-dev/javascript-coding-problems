8260bd11bb302f32c2f061961ffe4c4ad5e8780b
rashel9255 <ibrahim2000167@gmail.com>
2026-01-29 08:12:44 +0600

    personal: find largest number in an array by loop

--- Personal/3.findLargestNumber.js
+++ Personal/3.findLargestNumber.js
@@ -1,5 +1,19 @@
 // type: 1
+// const arr = [5, 12, 8, 20, 3];
+// const max = Math.max(...arr);
+
+// console.log(max);
+
+// type: 2
+
 const arr = [5, 12, 8, 20, 3];
-const max = Math.max(...arr);
+
+let max = arr[0];
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
 
 console.log(max);
